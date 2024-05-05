4086db87863f4fda1ee3774e66f3ac948fd6e954
Okute <okute@Okutes-MacBook-Air.local>
2024-05-05 12:52:17 +0300

    tailwind colour config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
         "hero-mobile": "url('/images/mobile/image-hero.jpg')",
         "hero-desktop": "url('/images/desktop/image-hero.jpg')",
       },
+      colors: {
+        "dark-gray": "hsl(0, 0%, 55%)",
+        "very-dark-gray": "hsl(0, 0%, 41%)",
+      },
     },
   },
   plugins: [],
