af7283ef55c0a8d5df7b5c559bff3aa5f18e0560
Okute <okute@Okutes-MacBook-Air.local>
2024-05-09 14:13:01 +0300

    get gallery from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ module.exports = {
       backgroundImage: {
         "hero-mobile": "url('/images/mobile/image-hero.jpg')",
         "hero-desktop": "url('/images/desktop/image-hero.jpg')",
+
+        // Desktop
+        "desktop-deep-earth": "url('/images/desktop/image-deep-earth.jpg')",
+        "desktop-curiosity": "url('/images/desktop/image-curiosity.jpg')",
+        "desktop-fisheye": "url('/images/desktop/image-fisheye.jpg')",
+        "desktop-from-above": "url('/images/desktop/image-from-above.jpg')",
+        "desktop-grid": "url('/images/desktop/image-grid.jpg')",
+        "desktop-interactive": "url('/images/desktop/image-interactive.jpg')",
+        "desktop-night-arcade": "url('/images/desktop/image-night-arcade.jpg')",
+        "desktop-pocket-borealis":
+          "url('/images/desktop/image-pocket-borealis.jpg')",
+        "desktop-soccer-team": "url('/images/desktop/image-soccer-team.jpg')",
       },
       colors: {
         "dark-gray": "hsl(0, 0%, 55%)",
